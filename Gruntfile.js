bdb56e0c07b43343377e9cf4c6b2df77cf9b3b8e
Hongxu Liu <djshu.us@gmail.com>
2014-10-19 12:33:22 -0700

    Basic layout

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            server: {
+                options: {
+                    port: 3000,
+                    hostname: 'localhost',
+                    open: {
+                        target: 'http://localhost:3000'
+                    },
+                    livereload: true
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            scripts: {
+                files: ['js/*.js']
+            },
+            css: {
+                files: ['css/*.css']
+            },
+            html: {
+                files: ['*.html']
+            },
+            content: {
+                files: ['img/**.*']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['connect', 'watch']);
+};
---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('#title').hide();
+
+$('#logo-container').on('mouseenter', function() {
+    $('#title').fadeToggle('fast');
+});
+
+$('#logo-container').on('mouseleave', function() {
+    $('#title').fadeToggle('fast');
+});
+
